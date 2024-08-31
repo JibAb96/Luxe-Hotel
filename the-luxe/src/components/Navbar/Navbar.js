083fcd48a71a3679a1c0417b068742eaaca5f411
JibAb96 <j.abshir96@gmail.com>
2024-08-31 10:02:57 +0000

    Style Navbar

--- the-luxe/src/components/Navbar/Navbar.js
+++ the-luxe/src/components/Navbar/Navbar.js
@@ -1,27 +1,29 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import {Navbar, Nav, Container, NavDropdown} from "react-bootstrap"
+import "./Navbar.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons'
+
+const element = <FontAwesomeIcon icon={faUser} />
 
 function Menu() {
+  
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="custom-navbar">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home" className="logo">The Luxe</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
+          <Nav className="nav-links ms-auto">
+            <Nav.Link href="#home" className="nav-item">HOME</Nav.Link>
+            <Nav.Link href="#link" className="nav-item">ROOMS</Nav.Link>
+            <Nav.Link href="#link" className="nav-item">ABOUT</Nav.Link>
+            <Nav.Link href="#link" className="nav-item book">BOOK</Nav.Link>
+            <NavDropdown title={element} id="basic-nav-dropdown" className="nav-item fs-5">
+              <NavDropdown.Item href="#action/3.1">
+                Register
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
+              <NavDropdown.Item href="#action/3.2">
+                Sign in
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
