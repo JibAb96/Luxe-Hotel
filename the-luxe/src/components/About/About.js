f7a607498e332714c7f8735f2000cea77265fb8c
JibAb96 <118972033+JibAb96@users.noreply.github.com>
2024-09-08 09:21:58 +0000

    Styled About component

--- the-luxe/src/components/About/About.js
+++ the-luxe/src/components/About/About.js
@@ -1,11 +1,26 @@
 import React from "react";
-import {Container, Row} from "react-bootstrap"
+import {Col, Container, Row, Image} from "react-bootstrap"
+import Reception from "../../images/hotel-reception-2.webp"
+import "./About.css"
 
 const About = () => {
     return(
-        <Container>
-            <Row className="reception">
-
+        <Container  fluid>
+            <h1 className="about-us">About us</h1>
+            <Row className="about">
+                <Col>
+                    <p className="about-us-p">
+                        Located in Luxembourg, our luxury hotel offers 
+                        an exquisite escape for discerning travelers. With elegantly 
+                        designed rooms, world-class amenities, and personalized service, 
+                        we cater to guests seeking the ultimate in comfort and sophistication. 
+                        Whether visiting for leisure or business, our hotel combines modern luxury with 
+                        the charm of Luxembourg’s rich heritage, creating an unforgettable experience.
+                    </p>
+                </Col>
+                <Col>
+                    <Image className="about-img" src={Reception}/>
+                </Col>
             </Row>
         </Container>
     )
