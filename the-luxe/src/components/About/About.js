dab6e50420a068890d5125adef98fdd5220cc92b
JibAb96 <118972033+JibAb96@users.noreply.github.com>
2024-09-08 08:38:29 +0000

    Create About component for About page

---
+++ the-luxe/src/components/About/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Container, Row} from "react-bootstrap"
+
+const About = () => {
+    return(
+        <Container>
+            <Row className="reception">
+
+            </Row>
+        </Container>
+    )
+}
+
+export default About
--- the-luxe/src/components/Hero/Hero.js
+++ the-luxe/src/components/Hero/Hero.js
@@ -1,20 +1,20 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import "./Hero.css"
 import TransparentButton from "../Buttons/TransparentButton";
 const Hero = () => {
     return (
-        <div className="hero">
-            <Container fluid>
-                <div class="hero__text-box">
-                    <h1 class="hero__text">
-                        <span class="hero__text-top">Luxury</span>
-                        <span class="hero__text-bottom">Comfort</span>
+        
+            <Container className="hero" fluid>
+                <Row className="hero__text-box">
+                    <h1 className="hero__text">
+                        <span>Luxury</span>
+                        <span>Comfort</span>
                     </h1>
-                    <TransparentButton>Book a Room</TransparentButton>
-                </div>
+                    <TransparentButton style={{marginLeft: "1rem"}}>Book a Room</TransparentButton>
+                </Row>
             </Container>
-        </div>
+        
     )
 }
 
