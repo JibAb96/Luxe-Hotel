543b429169e8d876360ad8b201ef10d7ffea8309
JibAb96 <118972033+JibAb96@users.noreply.github.com>
2024-09-26 05:16:14 +0000

    Create express server, add a get root and confirm server start with console.log

--- server.js
+++ server.js
@@ -11,3 +11,10 @@ app.use(cors());
 const { json } = pkg;
 
 app.use(json());
+
+app.get("/", (req, res) => { res.send("Hello World") });
+
+app.listen(3000,() => {
+    console.log("Server is running on port 3000");
+})
+  
