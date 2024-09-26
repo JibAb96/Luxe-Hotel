60452fb95f1c73ff9d337e726a64d0cd23c5c5a7
JibAb96 <118972033+JibAb96@users.noreply.github.com>
2024-09-26 09:47:55 +0000

    Test sign in post request with data written on server

--- server.js
+++ server.js
@@ -4,17 +4,36 @@ import pkg from "body-parser";
 import knex from "knex";
 import bcrypt from "bcrypt";
 
+const db = {
+    username: "Arnold",
+    password: "1234"
+}
+
 const app = express();
 
 app.use(cors());
+
+app.use(express.json());
 
 const { json } = pkg;
 
 app.use(json());
 
-app.get("/", (req, res) => { res.send("Hello World") });
+app.get("/", (req, res) => { res.send("success") });
+
+
+app.post("/signin", (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return res.status(400).json("incorrect form submission");
+    }
+    if (username === db.username && password === db.password) {
+        res.json("success");
+    } else {
+        res.status(400).json("error logging in");
+    }
+})
 
 app.listen(3000,() => {
     console.log("Server is running on port 3000");
 })
-  
