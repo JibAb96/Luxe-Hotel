47ebd45b5b9d9095f7139311409c82124ec489e8
JibAb96 <118972033+JibAb96@users.noreply.github.com>
2024-09-26 10:22:24 +0000

    import from pg and configure pool to postgres database

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ import knex from "knex";
 import bcrypt from "bcrypt";
 import handleRegistration from "./controllers/handleRegistration";
 import handleSignIn from "./controllers/handleSignIn";
+import { Pool } from "pg";
 
-const db = {
-    username: "Arnold",
-    password: "1234"
-}
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "Luxe-Hotel",
+    password: "Jibstaar96",
+    port: 5432
+});
 
 const app = express();
 
@@ -23,8 +27,9 @@ app.use(json());
 
 app.get("/", (req, res) => { res.send("success") });
 
+app.post("/register", handleRegistration(db, bcrypt));
 
-app.post("/signin", handle)
+app.post("/signin", handleSignIn(db, bcrypt));
 
 app.listen(3000,() => {
     console.log("Server is running on port 3000");
