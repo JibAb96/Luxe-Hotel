2ca4681d3f6e4c320a9921bf0aeeca76448b15e8
JibAb96 <j.abshir96@gmail.com>
2024-09-28 11:00:57 +0200

    Create simple async function that check req credential match database login data

--- controllers/handleSignIn.js
+++ controllers/handleSignIn.js
@@ -1,5 +1,43 @@
-const handleSignIn = (req, res, pool, bcrypt) => {
+const handleSignIn = async (req, res, pool, bcrypt) => {
+    const {email, password} = req.body;
+    if (!email || !password){
+        return res.status(400).json("email and password required");
+    }
 
+    const saltRounds = 10;
+
+    const hash = await bcrypt.hash(password, saltRounds);
+
+    try {
+        await pool.query('BEGIN');
+        
+        const loginQuery = 'SELECT email, hash FROM login WHERE email = $1';
+        const loginResults = await pool.query(loginQuery, [email])
+
+        if(loginResults.rows.length > 0){
+            const userData = loginResults.rows[0];
+            const isValid = bcrypt.compareSync(password, userData.hash)
+
+            if(isValid){
+                const profileQuery = 'SELECT * FROM profiles WHERE email = $1';
+                const profileResult = await pool.query(profileQuery, [email])
+
+                if(profileResult.rows.length > 0){
+                    res.json(profileResult.rows[0])
+                } else{
+                    res.status(400).json('Unable to get user')
+                }
+            } else {
+                res.status(400).json('Wrong credentials')
+            }
+        } else{
+            res.status(400).json('Unable to get user')
+        }
+
+    } catch (err) {
+        console.error('Database query error:', err);
+        res.status(500).json("Internal server error");
+    }
 };
 
 export default handleSignIn;
