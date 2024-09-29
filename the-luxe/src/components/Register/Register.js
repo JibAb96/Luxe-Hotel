1c005ed3117bbe373215c66a141bb7133348001b
JibAb96 <j.abshir96@gmail.com>
2024-09-29 07:34:21 +0200

    Change address for api fetch to local host

--- the-luxe/src/components/Register/Register.js
+++ the-luxe/src/components/Register/Register.js
@@ -102,7 +102,7 @@ const Register = () => {
         };
 
         try {
-            const response = await fetch("https://obscure-rotary-phone-56v6qv4999wfw9x-3000.app.github.dev/register", {
+            const response = await fetch("http://localhost:3000/register", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
