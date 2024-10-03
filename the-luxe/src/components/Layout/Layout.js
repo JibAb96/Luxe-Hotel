e4788f40b8f4d219b3b90a1d5384adafb15a2d32
JibAb96 <j.abshir96@gmail.com>
2024-10-03 14:56:39 +0200

    Remove isSignedIn and setIsSignedIn prop

--- the-luxe/src/components/Layout/Layout.js
+++ the-luxe/src/components/Layout/Layout.js
@@ -2,10 +2,11 @@ import React from "react";
 import Navigation from "../Navigation/Navigation";
 import Footer from "../Footer/Footer";
 import "./Layout.css"
-const Layout = ({children, isSignedIn, setIsSignedIn}) => {
+const Layout = ({children}) => {
+        
     return(
         <div className="wrapper">
-            <Navigation isSignedIn={isSignedIn} setIsSignedIn={setIsSignedIn}/>
+            <Navigation/>
             <div className="content">
                 {children}
             </div>
