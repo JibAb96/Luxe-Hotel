18c4aef50575fd1b01949aad0e973af3c3b151bb
JibAb96 <j.abshir96@gmail.com>
2024-10-03 14:53:45 +0200

    Insert DeleteProfile component if the user clicks delete button, using state and handleSwitch function

--- the-luxe/src/components/Profile/Profile.js
+++ the-luxe/src/components/Profile/Profile.js
@@ -1,53 +1,67 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import TextCard from '../Cards/TextCard';
 import './Profile.css';
 import TransparentButton from '../Buttons/TransparentButton';
 import { ProfileContext } from '../../contexts/ProfileContext';
+import DeleteProfile from '../Delete-User/DeleteProfile';
+
 const Profile = () => {
+    const [showDeleteProfile, setShowDeleteProfile] = useState(false);
+    
+    const handleSwitch = () => {
+        setShowDeleteProfile(true)
+    }
     const { profileData } = useContext(ProfileContext)
+    
     const dob = profileData.date_of_birth.substring(0, profileData.date_of_birth.indexOf("T"))
     const joined = profileData.joined.substring(0, profileData.joined.indexOf("T"))
 
     return (
-        <Container style={{padding: "3rem 0", overflowX:"hidden"}}fluid>
-            {console.log(profileData)}
-            <h1 className='profile-title'>Profile</h1>
-            <Row className='d-flex justify-content-center' style={{}}>
-                <TextCard
-                    lg={6}
-                    style={{ width: '30rem', minHeight: '10rem', }}
-                    Title={profileData.first_name}
-                    Text={
-                        <>
-                        <p><span className='bold'>Full Name:</ span> {profileData.first_name}</p>
-                        <p><span className='bold'>Date of Birth:</span> {dob}</p>
-                        <p><span className='bold'>Joined:</span> {joined}</p>
-                        </>}
-                    titleClass={"titleClass"}
-                    Element={
-                                <div>
-                                    <TransparentButton style={{minWidth:"7rem", backgroundColor:"#455d58" }}>Edit</TransparentButton>
-                                    <TransparentButton style={{minWidth:"7rem", backgroundColor:"#455d58" }}>Delete</TransparentButton>
-                                </div>
-                            }      
-                />
-                <TextCard
-                    lg={6}
-                    style={{ width: '30rem', minHeight: '10rem' }}
-                    Text={
-                        <>
-                            <p><span className='bold'>Email:</ span> {profileData.email}</p>
-                            <p><span className='bold'>Phone:</span> {profileData.phone}</p>
-                            <p><span className='bold'>Country:</span> {profileData.country}</p>
-                            <p><span className='bold'>City:</ span> {profileData.city}</p>
-                            <p><span className='bold'>Address:</span> {profileData.address}</p>
-                            <p><span className='bold'>Postal Code:</span> {profileData.postal_code}</p>
-                        </>
-                        }
-                />
-            </Row>
-        </Container>
+        <>
+            {  showDeleteProfile ? 
+                    <DeleteProfile /> :
+                    <Container style={{padding: "3rem 0", overflowX:"hidden"}}fluid>
+                        {console.log(profileData)}
+                        <h1 className='profile-title'>Profile</h1>
+                        <Row className='d-flex justify-content-center' style={{}}>
+                            <TextCard
+                                lg={6}
+                                style={{ width: '30rem', minHeight: '10rem', }}
+                                Title={profileData.first_name}
+                                Text={
+                                    <>
+                                    <p><span className='bold'>Full Name:</ span> {profileData.first_name}</p>
+                                    <p><span className='bold'>Date of Birth:</span> {dob}</p>
+                                    <p><span className='bold'>Joined:</span> {joined}</p>
+                                    </>}
+                                titleClass={"titleClass"}
+                                Element={
+                                            <div>
+                                                <TransparentButton style={{minWidth:"7rem", backgroundColor:"#455d58" }}>Edit</TransparentButton>
+                                                <TransparentButton 
+                                                style={{minWidth:"7rem", backgroundColor:"#455d58" }} 
+                                                onClick={handleSwitch}>Delete</TransparentButton>                                </div>
+                                        }      
+                            />
+                            <TextCard
+                                lg={6}
+                                style={{ width: '30rem', minHeight: '10rem' }}
+                                Text={
+                                    <>
+                                        <p><span className='bold'>Email:</ span> {profileData.email}</p>
+                                        <p><span className='bold'>Phone:</span> {profileData.phone}</p>
+                                        <p><span className='bold'>Country:</span> {profileData.country}</p>
+                                        <p><span className='bold'>City:</ span> {profileData.city}</p>
+                                        <p><span className='bold'>Address:</span> {profileData.address}</p>
+                                        <p><span className='bold'>Postal Code:</span> {profileData.postal_code}</p>
+                                    </>
+                                    }
+                            />
+                        </Row>
+                    </Container>
+            }
+        </>
     );
 };
 
