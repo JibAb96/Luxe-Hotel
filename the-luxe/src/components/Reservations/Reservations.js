5f34a9f4b345bb286d12727bf447eb3788d89fe8
JibAb96 <j.abshir96@gmail.com>
2024-10-05 18:12:34 +0200

    Connect reservations component with back-end and display booking info from database

--- the-luxe/src/components/Reservations/Reservations.js
+++ the-luxe/src/components/Reservations/Reservations.js
@@ -1,25 +1,52 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Container, Row } from "react-bootstrap";
 import TextCard from "../Cards/TextCard";
 import "./Reservations.css"
 import TransparentButton from "../Buttons/TransparentButton";
+import { ProfileContext } from "../../contexts/ProfileContext";
 
-const Reservations = ({ reservations }) => {
+const Reservations = () => {
+
+    const [bookings, setBookings] = useState([]);
+
+    const { profileData } = useContext(ProfileContext)
+    useEffect(()  => {
+        if( !profileData.id ){ return }
+        const fetchBookings = async () =>  {
+        try {
+            const response = await fetch(`http://localhost:3000/reservations/${profileData.id}`)
+            if(!response.ok){
+                throw new Error("Failed to fetch bookings")
+            }
+            const data = await response.json();
+            setBookings(data);
+        }
+        catch(error){
+            console.error(error)
+        }}
+        fetchBookings();
+    }, [profileData.id, bookings])
+    
+
     return (
-        <Container fluid>
+        <Container style={{padding: "1rem"}} fluid>
+            {console.log("Bookings:", bookings)}
             <Row className="d-flex justify-content-center">
                 <h1 className="page-heading">Your Reservations</h1>
-                {reservations.map((reservation, index) => (
+             </Row>
+             <Row className="d-flex justify-content-center" style={{padding: "2rem 0 7rem"}}>   
+                {bookings.map((booking, index) => (
                     <TextCard 
                         key={index}
-                        Title={<span className="heading">{reservation.room}</span>}
+                        titleClass={"heading"}
+                        Title={booking.room_type}
                         Text={
                             <>
-                                <p><span className="bold">Booking Id:</span> {reservation.id}</p>
-                                <p><span className="bold">Check-In</span>: {reservation.checkin}</p>
-                                <p><span className="bold">Check-Out:</span> {reservation.checkout}</p>
-                                <p><span className="bold">Guests:</span> {reservation.guests}</p>
-                                <p><span className="bold">Total-price:</span> €{reservation.price}</p>
+                                <p><span className="bold">Booking Id:</span> {booking.id}</p>
+                                <p><span className="bold">Check-In</span>: {booking.check_in.substring(0,10)}</p>
+                                <p><span className="bold">Check-Out:</span> {booking.check_out.substring(0,10)}</p>
+                                <p><span className="bold">Guests:</span> {booking.guests}</p>
+                                <p><span className="bold">Total-price:</span> €{booking.price}</p>
                             </>
                             }
                             Element={
