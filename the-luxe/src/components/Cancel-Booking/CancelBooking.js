de97d8d1b2222c4be03922d8a60887f9fb2ebdaf
JibAb96 <j.abshir96@gmail.com>
2024-10-06 17:33:06 +0200

    Create cancel booking component that makes a delete request to the back end, deleting the target booking. Handle visibilty and UI update through props.

---
+++ the-luxe/src/components/Cancel-Booking/CancelBooking.js
@@ -0,0 +1,69 @@
+import React, { useContext } from "react";
+import { Container, Row } from "react-bootstrap";
+import TransparentButton from "../Buttons/TransparentButton";
+import { AlertContext } from "../../contexts/Alert";
+import "./CancelBooking.css"
+
+const CancelBooking = ({ booking, handleExit, onDeletion }) => {
+     
+    const {showAlertWithTimeout} = useContext(AlertContext);
+    
+    const deleteBooking = async () => {    
+        try {
+            const response = await fetch(`http://localhost:3000/delete-booking/${booking.id}`, {
+                method: "DELETE"
+            })
+            const data = await response.json();
+            if(data === "Booking was deleted successfully"){
+                handleExit();
+                onDeletion(booking);
+                showAlertWithTimeout("You have successfully deleted your booking!", "alert-success")
+            } else{
+                handleExit();
+                showAlertWithTimeout("An error occurred. Please try again.", "alert-danger")
+            }
+        }
+        catch (error){
+            console.error("Error logging in:", error);
+            showAlertWithTimeout("An error occurred. Please try again.","alert-danger")
+        }
+        
+    }
+    
+    return (
+        <>
+            <Container className="delete-booking" fluid>
+                <button 
+                            type="button" 
+                            className="exit-button" 
+                            onClick={handleExit} 
+                            aria-label="Close"
+                        >
+                        &times;
+                </button>            
+                <h1 className="delete-booking--title">Cancel Reservation</h1>
+                <p className="delete-booking-p">Are you sure you want to delete your 
+                    <span className="bold"> Reservation</span>?</p>
+                <Row>
+                    <TransparentButton 
+                        onClick={deleteBooking}
+                        style={{backgroundColor: "#800020", marginBottom: "1rem"}}
+                    >
+                        Delete
+                    </TransparentButton>
+                    <TransparentButton 
+                        onClick={handleExit}
+                        style={{backgroundColor: "#455d58"}}
+                    >
+                        Keep
+                    </TransparentButton>
+                </Row>
+                <h1 className="delete-booking-title"> We are sorry to see you go!</h1>
+                <p className="delete-booking-p">If there is anything we can do to assist
+                     you in the future, please don't hesitate to reach out.</p>
+            </Container>
+        </>
+    )
+}
+
+export default CancelBooking;
