5ad24442f6dbebccaa5b136f34321deafc8ec378
JibAb96 <j.abshir96@gmail.com>
2024-10-13 09:59:35 +0200

    Add function to fetch booking details to display.

--- the-luxe/src/components/Confirm-Booking/ConfirmBooking.js
+++ the-luxe/src/components/Confirm-Booking/ConfirmBooking.js
@@ -1,17 +1,43 @@
-import React, { useEffect } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { Container } from "react-bootstrap"
 import { useParams } from "react-router-dom"
-
+import { ProfileContext } from "../../contexts/ProfileContext"
 const ConfirmBooking = () => {
+    
     const { id } = useParams();
+
+    const [haveData, setHaveData] = useState(false);
+    const [booking, setBooking] = useState(null);
+
+    const { profileData } = useContext(ProfileContext);
+
+    useEffect(() => {
+        const fetchBooking = async () => {
+            try{
+                const response = await fetch(`http://localhost:3000/confirm-booking/${id}`)
+                if (!response.ok){
+                    setHaveData(false);
+                    return;
+                }
+
+                const data = response.json();
+                setBooking(data)
+            }
+            catch(error){
+                console.error(error)
+                setHaveData(false)
+            }
+        }
+        fetchBooking();
+    }, [])
 
     return(
         <Container>
             <h1>Booking Successful</h1>
-            <p>Thanks for booking with us {name}!</p>
-            <p>Your booking reference is {Booking.id}.</p>
-            <p>You booked a {room-type} from {check-in} to {check-out} for {guests} 2.</p>
-            <p>Total price: {price}</p>
+            <p>Thanks for booking with us {profileData.first_name}!</p>
+            <p>Your booking reference is {booking.id}.</p>
+            <p>You booked a {booking.room_type} from {booking.check_in} to {booking.check_out} for {booking.guests} guests.</p>
+            <p>Total price: {booking.price}</p>
             <h1>We look forward to welcoming you to our hotel</h1>
             <p className="bold">Check in time is at 15:00 (3PM)</p>
             <p>If you have any quesion or need any support please contact us!</p>
