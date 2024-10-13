36e71e583485b61e859cf515fde4d752d04dfa38
JibAb96 <j.abshir96@gmail.com>
2024-10-13 16:22:54 +0200

    Create component for spinning loader.

---
+++ the-luxe/src/components/Spinner/Spinner.js
@@ -0,0 +1,13 @@
+import { Container, Spinner } from "react-bootstrap"
+
+const Loader = () => {
+    return(
+        <Container className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }} fluid>
+            <Spinner animation="border" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </Spinner>
+        </Container>
+        )
+}
+
+export default Loader
