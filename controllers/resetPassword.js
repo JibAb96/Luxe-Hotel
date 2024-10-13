23381c20a9004dc255d625d3d4f4c44dd0988428
JibAb96 <j.abshir96@gmail.com>
2024-10-13 08:54:45 +0200

    Write function for updating the password in the database with the password sent from the client-side.

---
+++ controllers/resetPassword.js
@@ -0,0 +1,30 @@
+const resetPassword = async (req, res, pool, bcrypt) => {
+    
+    const { id } = req.params;
+
+    const { password } = req.body;
+    
+    console.log(password);
+    
+    try{
+
+        const selectUser = await pool.query("SELECT * FROM login WHERE id = $1 AND reset_requested = true", [id]);
+
+        if(!selectUser.rows.length) {
+            return res.status(400).json({message: "Invalid or no reset request found"})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        console.log(hashedPassword)
+
+        await pool.query("UPDATE login SET hash = $1, reset_requested = false WHERE id = $2", [hashedPassword, id]);
+
+        res.json({ message: "Password has been reset successfully" });
+        
+    } catch (error){
+        res.status(500).json({ message: error })
+    }
+}
+
+export default resetPassword
