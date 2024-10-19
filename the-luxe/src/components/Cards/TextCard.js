4aa43c2184d086846090869b2b77cb6d9d3b91f9
JibAb96 <j.abshir96@gmail.com>
2024-10-19 09:31:46 +0200

    Allow for the addition of class names for Card element that can be passed through props.

--- the-luxe/src/components/Cards/TextCard.js
+++ the-luxe/src/components/Cards/TextCard.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Card, Col } from "react-bootstrap";
 import "./TextCard.css";
-const TextCard = ({ Title, Text, Element, titleClass, lg = 4, ...rest }) => {
+const TextCard = ({ Title, Text, Element, titleClass, lg = 4, className, ...rest }) => {
   return (
     <Col xs={12} sm={6} lg={lg} className="d-flex justify-content-center">
-      <Card className="text-card shadow p-1 rounded" {...rest}>
+      <Card className={`text-card shadow p-1 rounded ${ className || ""}`} {...rest}>
         <Card.Body className="structure-card-body ">
           <Card.Title className={titleClass}>{Title}</Card.Title>
           <Card.Text>{Text}</Card.Text>
