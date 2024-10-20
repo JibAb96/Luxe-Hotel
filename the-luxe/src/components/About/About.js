8948ec066ee5ec3c96dbb101b1f1e8656beffec0
JibAb96 <j.abshir96@gmail.com>
2024-10-20 08:26:08 +0200

    Remove hard coded about us section and bring in about us component.

--- the-luxe/src/components/About/About.js
+++ the-luxe/src/components/About/About.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Col, Container, Row, Image } from "react-bootstrap";
-import Reception from "../../images/hotel-reception-2.webp";
+import { Container, Row } from "react-bootstrap";
 import "./About.css";
+import AboutSection from "../About-Section/AboutSection";
 import DiningSection from "../Dining-Section/Dining-Section";
 import ReserveSection from "../Reserve-Section/Reserve-Section";
 
@@ -9,27 +9,19 @@ const About = ({ isSignedIn }) => {
   return (
     <Container fluid>
       <h1 className="about-us">About us</h1>
-      <Row className="about">
-        <Col xs={12} md={6}>
-          <p className="about-us-p">
-            Located in Luxembourg, our luxury hotel offers an exquisite escape
-            for discerning travelers. With elegantly designed rooms, world-class
-            amenities, and personalized service, we cater to guests seeking the
-            ultimate in comfort and sophistication. Whether visiting for leisure
-            or business, our hotel combines modern luxury with the charm of
-            Luxembourg’s rich heritage, creating an unforgettable experience.
-          </p>
-        </Col>
-        <Col xs={12} md={6}>
-          <Image className="about-img" src={Reception} fluid />
-        </Col>
-      </Row>
-      <Row>
-        <DiningSection />
-      </Row>
-      <Row>
-        <ReserveSection isSignedIn={isSignedIn} />
-      </Row>
+      <section>
+        <AboutSection />
+      </section>
+      <section>  
+        <Row>
+          <DiningSection />
+        </Row>
+       </section> 
+      <section>  
+        <Row>
+          <ReserveSection isSignedIn={isSignedIn} />
+        </Row>
+      </section> 
     </Container>
   );
 };
