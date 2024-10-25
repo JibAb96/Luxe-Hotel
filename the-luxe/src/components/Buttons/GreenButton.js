fe4cbaca34aaa38163da1939db0a1b1a183ff860
JibAb96 <j.abshir96@gmail.com>
2024-10-25 08:13:44 +0200

    Add prop for adding aria-label attribute to GreenButton.

--- the-luxe/src/components/Buttons/GreenButton.js
+++ the-luxe/src/components/Buttons/GreenButton.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import "./GreenButton.css";
 
-const GreenButton = ({ children, type, disabled, className, ...rest }) => {
+const GreenButton = ({ children, type, ariaLabel = "button", disabled, className, ...rest }) => {
   return (
     <Button 
       className={`button ${className || ""}`} 
       {...rest} 
       type={type}
       disabled={disabled}
+      aria-label={ariaLabel}
     >
       {children}
     </Button>
