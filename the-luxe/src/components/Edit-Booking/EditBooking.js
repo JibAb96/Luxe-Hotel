0067156c182ffb96bcaa4e904f0ecf9dcbd4ec90
JibAb96 <j.abshir96@gmail.com>
2024-10-26 07:15:13 +0200

    Add isLoading logic for better UX, add alerts for better user feedback, more detail error messaging and put form validation logic into one function.

--- the-luxe/src/components/Edit-Booking/EditBooking.js
+++ the-luxe/src/components/Edit-Booking/EditBooking.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Form, Card, Row } from "react-bootstrap";
+import { Form, Card, Row, Alert } from "react-bootstrap";
 import GreenButton from "../Buttons/GreenButton";
 import "./EditBooking.css";
 import FormInput from "../Form/Input";
@@ -10,8 +10,9 @@ const EditBooking = ({ booking, handleExit, onUpdate }) => {
   const [checkOut, setCheckOut] = useState("");
   const [roomType, setRoomType] = useState("");
   const [guests, setGuests] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  const { showAlertWithTimeout } = useContext(AlertContext);
+  const { showAlertWithTimeout, showAlert, alertMessage, alertStyle } = useContext(AlertContext);
 
   useEffect(() => {
     setCheckIn(booking.check_in);
@@ -19,10 +20,6 @@ const EditBooking = ({ booking, handleExit, onUpdate }) => {
     setRoomType(booking.room_type);
     setGuests(booking.guests);
   }, [booking]);
-
-  const setChange = (e, setState) => {
-    setState(e.target.value);
-  };
 
   const prices = { standard: 120, deluxe: 150, suite: 200 };
 
@@ -40,28 +37,72 @@ const EditBooking = ({ booking, handleExit, onUpdate }) => {
     return `${year}-${month}-${day}`;
   };
 
-  const checkInInput = formatDateForInput(checkIn);
-  const checkOutInput = formatDateForInput(checkOut);
+  const formattedCheckIn = formatDateForInput(checkIn);
+  const formattedCheckOut = formatDateForInput(checkOut);
+
+  const validateForm = (checkIn, checkOut, roomType, guests) => {
+    const currentDate = new Date();
+    currentDate.setHours(0, 0, 0, 0);
+    if (isNaN(checkIn.getTime()) || isNaN(checkOut.getTime())) {
+      setIsLoading(false);
+
+      showAlertWithTimeout("Please provide valid dates.", "alert-danger");
+      return false;
+    }
+
+    if (checkIn >= checkOut) {
+      setIsLoading(false);
+      showAlertWithTimeout(
+        "Check-in date must be before check-out date.",
+        "alert-danger",
+      );
+      return false;
+    }
+
+    if (checkIn < currentDate) {
+      setIsLoading(false);
+      showAlertWithTimeout(
+        "Check-in date cannot be in the past",
+        "alert-danger",
+      );
+      return false;
+    }
+
+    if ((roomType === "deluxe" || roomType === "standard") && guests > 2) {
+      setIsLoading(false);
+      showAlertWithTimeout(
+        "Maximum guests for selected room is 2",
+        "alert-danger",
+      );
+      return false;
+    } else if (guests > 4) {
+      setIsLoading(false);
+      showAlertWithTimeout("Maximum guests for the suite is 4", "alert-danger");
+      return false;
+    } else if (guests <= 0) {
+      setIsLoading(false);
+      showAlertWithTimeout(
+        "Please state how many guests this booking is for",
+        "alert-danger",
+      );
+      return false;
+    }
+
+    return true;
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    
+    setIsLoading(true);
 
     const checkInDate = new Date(checkIn);
     const checkOutDate = new Date(checkOut);
     const utcCheckIn = checkInDate.toISOString();
     const utcCheckOut = checkOutDate.toISOString();
 
-    if (isNaN(checkInDate.getTime()) || isNaN(checkOutDate.getTime())) {
-      showAlertWithTimeout("Please provide valid dates.", "alert-danger");
-      return;
-    }
-
-    if (checkInDate >= checkOutDate) {
-      showAlertWithTimeout(
-        "Check-in date must be before check-out date.",
-        "alert-danger",
-      );
-      return;
+    if(!validateForm(checkInDate, checkOutDate, roomType, guests)){
+      return
     }
 
     const price = prices[roomType] * calculateDaysBetween(checkIn, checkOut);
@@ -77,6 +118,7 @@ const EditBooking = ({ booking, handleExit, onUpdate }) => {
     const apiURL = process.env.REACT_APP_API_BASE_URL;
 
     try {
+      
       const response = await fetch(`${apiURL}/edit-booking/${booking.id}`, {
         method: "PUT",
         headers: { "Content-type": "application/json" },
@@ -86,7 +128,7 @@ const EditBooking = ({ booking, handleExit, onUpdate }) => {
       if (!response.ok) {
         handleExit();
         return showAlertWithTimeout(
-          "There was an error making your booking.",
+          response.status || "There was an error making your booking.",
           "alert-danger",
         );
       }
@@ -101,19 +143,26 @@ const EditBooking = ({ booking, handleExit, onUpdate }) => {
         price,
       };
       onUpdate(updatedBooking);
-      showAlertWithTimeout("Your booking was successful!", "alert-success");
+      showAlertWithTimeout("Your booking was successfully updated!", "alert-success");
     } catch (error) {
       handleExit();
       console.error(error.message);
       showAlertWithTimeout(
-        "There was an error making your booking",
+        error.message || "There was an error making your booking",
         "alert-danger",
       );
+    } finally{
+      setIsLoading(false);
     }
   };
 
   return (
     <div>
+      {showAlert && (
+          <Alert className={`alert ${alertStyle}`} role="alert">
+            {alertMessage}
+          </Alert>
+        )}
       <Card className="edit-booking">
         <Form
           className="shadow p-4 rounded edit-booking-form"
@@ -131,15 +180,15 @@ const EditBooking = ({ booking, handleExit, onUpdate }) => {
           <FormInput
             label={"Check-in"}
             type={"date"}
-            value={checkInInput}
-            onChange={(e) => setChange(e, setCheckIn)}
+            value={formattedCheckIn}
+            onChange={(e) => setCheckIn(e.target.value)}
             required
           />
           <FormInput
             label={"Check-out"}
             type={"date"}
-            value={checkOutInput}
-            onChange={(e) => setChange(e, setCheckOut)}
+            value={formattedCheckOut}
+            onChange={(e) => setCheckOut(e.target.value)}
             required
           />
           <Form.Group className="mb-2" controlId="Room-Type">
@@ -148,7 +197,7 @@ const EditBooking = ({ booking, handleExit, onUpdate }) => {
               aria-label="Room-Type"
               as="select"
               value={roomType}
-              onChange={(e) => setChange(e, setRoomType)}
+              onChange={(e) => setRoomType(e.target.value)}
               required
             >
               <option></option>
@@ -167,15 +216,23 @@ const EditBooking = ({ booking, handleExit, onUpdate }) => {
             label={"Guests"}
             type={"number"}
             value={guests}
-            onChange={(e) => setChange(e, setGuests)}
+            onChange={(e) => setGuests(e.target.value)}
             required
           />
           <Row className="d-flex justify-content-center">
             <GreenButton
               type="submit"
               className={"m-1"}
+              disabled={isLoading}
             >
-              Book
+              {isLoading ? (
+                  <>
+                    <span className="spinner-border spinner-border-sm me-2" />
+                    Booking...
+                  </>
+                  ) : (
+                  'Book'
+                  )}
             </GreenButton>
           </Row>
         </Form>
