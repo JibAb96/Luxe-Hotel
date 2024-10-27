bf3c76a9214dd5f3f8b29d8b84503b5b54a161b3
JibAb96 <j.abshir96@gmail.com>
2024-10-27 11:27:46 +0100

    Initially connect with database then finally release, make conditional more clear and give a more error messeage.

--- controllers/confirmBooking.js
+++ controllers/confirmBooking.js
@@ -1,14 +1,20 @@
 const confirmBookings = async (req, res, pool) => {
     const { id } = req.params;
+    const client = await pool.connect();
     try{
-        const data = await pool.query("SELECT * FROM bookings WHERE id = $1", [id])
-        if (!data.rows.length > 0){
-            return res.status(500).json({message: "Booking doesn't exist"})
+        const data = await client.query("SELECT * FROM bookings WHERE id = $1", [id])
+        if (data.rows.length === 0){
+            return res.status(404).json({message: "Booking doesn't exist"})
         }
         res.json(data.rows[0])
     } 
     catch (error){
-        res.status(500).json({message: "Server Error"})
+        res.status(500).json({
+            message: "Server Error",
+            error: process.env.NODE_ENV === 'development' ? error.message : undefined
+        });
+    } finally {
+        client.release();
     }
 }
 
