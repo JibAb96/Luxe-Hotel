5b1e10dbb04b1ae7dd15359315e0d7c862c7c270
JibAb96 <j.abshir96@gmail.com>
2024-11-02 16:23:14 +0100

    Create an authentication token and send it to the back end.

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,3 +1,4 @@
+import { createAuthToken } from "./authentication.js";
 const handleSignIn = async (req, res, pool, bcrypt) => {
     const {email, password} = req.body;
     if (!email || !password){
@@ -25,13 +26,21 @@ const handleSignIn = async (req, res, pool, bcrypt) => {
         if(profileResult.rows.length === 0){
             return res.status(400).json({message: "Unable to get user profile" })
         }
-        res.json(profileResult.rows[0]);
+
+        const userId = await client.query("SELECT id FROM profiles WHERE email = $1", [email])
+        const token = createAuthToken(userId.rows[0].id);
+
+        res.json({
+            token,
+            "user": profileResult.rows[0]
+        });
     } catch (err) {
         console.error('Database query error:', err);
         res.status(500).json({ message:"Internal server error" });
     } finally {
         client.release();
     }
+
 
 
     
